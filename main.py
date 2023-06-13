4b0dfab989df7cdfcd20d0cf9ee64a3f7979da25
Wojciech-Mazurowski-digimuth <wojci123@gmail.com>
2023-06-13 22:51:21 +0200

    starting analisis thingy

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import Analyzer
 import time
 
 analyzer = Analyzer.Analyzer("./data/TCEC_data.csv", "./data/results.csv")
-
+print(f"Starting analysis...")
 start_time = time.time()
 analyzer.run_analysis()
 end_time = time.time()
-
 execution_time = end_time - start_time
 print(f"Analysis completed in {execution_time} seconds.")
