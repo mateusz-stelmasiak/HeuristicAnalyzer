6886451b40f88fa9dd1f5e54fe6aadf76b82841e
Wojciech-Mazurowski-digimuth <wojci123@gmail.com>
2023-06-17 19:01:42 +0200

    swine analyser analytic optimisation

--- SwineAnalyzer.py
+++ SwineAnalyzer.py
@@ -1,9 +1,16 @@
+import chess
 import pandas as pd
+
+from engines.Engine import Engine, EngineType
 
 
 class SwineAnalyzer:
 
     def __init__(self, sf_depth_limit):
+        self.board = chess.Board()
+        self.engine = Engine(EngineType.STOCKFISH)
+        self.limit = chess.engine.Limit(depth=sf_depth_limit)
+
         self.current_positions = {
             'whiteKing': 'e1', 'blackKing': 'e8',
             'whiteRooka': 'a1', 'whiteRookh': 'h1',
@@ -68,5 +75,52 @@ class SwineAnalyzer:
                                                                  '') and '2' in self.current_positions.get('blackRookh',
                                                                                                            ''):
                                 self.results['BlackDoubleSwine'] = True
+            # optimisation conditions
+            # if both player swined
+            if self.results['WhiteDoubleSwine'] and self.results['BlackDoubleSwine']:
+                break
+            # if all rooks are taken
+            if not any(rook in self.current_positions for rook in
+                       ['whiteRooka', 'whiteRookh', 'blackRooka', 'blackRookh']):
+                break
+
+            # For white rooks - if one rook was taken and
+            if (sum(rook in self.current_positions for rook in ['whiteRooka', 'whiteRookh']) == 1 and self.results[
+                'WhiteSingleSwine']) and (
+                    sum(rook in self.current_positions for rook in ['blackRooka', 'blackRookh']) == 1 and self.results[
+                'BlackSingleSwine']):
+                break
 
         return pd.DataFrame(self.results, index=[0])
+
+    def empirical_method(self, moves):
+        board = chess.Board()
+        for move in moves:
+            board.push_uci(move)
+            legal_moves = board.generate_legal_moves()
+            print(legal_moves)
+        # for move in moves:
+        #     # Create a new chess.Board object from the current move list
+        #     board = chess.Board()
+        #     for m in moves:
+        #         board.push_san(m)
+        #
+        #     # Generate a list of all legal moves
+        #     legal_moves = list(board.legal_moves)
+        #
+        #     # Filter the moves to only include those where a rook moves to the 7th (or 2nd) rank
+        #     rook_moves = [m for m in legal_moves if str(m)[1] == '7' and board.piece_at(m.from_square).symbol() == 'R'
+        #                   or str(m)[1] == '2' and board.piece_at(m.from_square).symbol() == 'r']
+        #
+        #     # Use the engine to evaluate the rook moves
+        #     for rook_move in rook_moves:
+        #         result = self.engine.play(board, self.limit)
+        #
+        #         # If the move is the best one according to Stockfish
+        #         if rook_move == result.move:
+        #             if str(rook_move)[1] == '7':
+        #                 self.results['WhiteSingleSwine'] = True
+        #             else:  # str(rook_move)[1] == '2'
+        #                 self.results['BlackSingleSwine'] = True
+        #
+        # return pd.DataFrame(self.results, index=[0])
